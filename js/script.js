5a412a69ef59ab59959a31804e9fc56eb60791f9
RomanTsvetik <85985444+RomanTsvetik@users.noreply.github.com>
2021-07-07 18:26:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let x = 4;
+let y = 9;
+let r = 10;
+
+let z = (Math.pow(x, 2) + Math.pow(y, 2));
+Math.sqrt(z) <= r ? console.log('Точка принадлежит окружности') : console.log('Точка находится за границей окружности');
